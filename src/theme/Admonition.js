c805550d7232cc53ca08e07195336f5093e309eb
zefan lin <xpesir@live.cn>
2023-07-29 20:12:33 +0000

    共告牌标题测试

--- src/theme/Admonition.js
+++ src/theme/Admonition.js
@@ -13,9 +13,15 @@ export default function AdmonitionWrapper(props) {
         </div>);
 
     case 'condition':
+
+      if(props.title == null)
+      {
+        props.title = '状态条件';
+      }
+
       return (
         <div class="theme-admonition theme-admonition-tip alert alert--success admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
-          <div class="admonitionHeading_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">状态条件</div>
+          <div class="admonitionHeading_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">{props.title}</div>
           <div class="admonitionContent_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">{props.children}</div>
         </div>);
     default:
