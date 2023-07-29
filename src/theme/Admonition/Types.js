698cc9cf52437760e33b0b49b26dba34155100fa
zefan lin <xpesir@live.cn>
2023-07-29 19:26:15 +0000

    命名规范

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
 
-function event(props) {
+function Event(props) {
   return (
     <div style={{border: 'solid red', padding: 10}}>
       <h5 style={{color: 'blue', fontSize: 30}}>{props.title}</h5>
@@ -10,7 +10,7 @@ function event(props) {
   );
 }
 
-function condition(props) {
+function Condition(props) {
     return (
         <div class="theme-admonition theme-admonition-tip alert alert--success admonition_LlT9">
         <div class="admonitionHeading_tbUL">状态条件</div>
@@ -24,8 +24,8 @@ const AdmonitionTypes = {
 
   // Add all your custom admonition types here...
   // You can also override the default ones if you want
-  'event': event,
-  'condition': condition,
+  'event': Event,
+  'condition': Condition,
 };
 
 export default AdmonitionTypes;
